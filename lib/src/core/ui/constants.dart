623b69ca028c2e8e09cc08beb9564cbd15f09ff5
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-08-15 17:15:25 -0300

    Making splash screen, login screen and repositorie for authentication

---
+++ lib/src/core/ui/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+sealed class FontConstants {
+  static const fontFamily = 'Poppins';
+}
+
+sealed class ColorsConstants {
+  static const brown = Color(0xFFB07B01);
+  static const gray = Color(0xFF999999);
+  static const lightGray = Color(0xFFB07B01);
+  static const red = Color(0xFFEB1212);
+}
